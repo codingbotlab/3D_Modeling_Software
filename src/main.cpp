33f847703d3a387e304a5d901ec1bb9ea2263a2e
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2023-12-14 15:45:32 +0530

    Create main.cpp
    Signed-off-by: CodingBot <133736959+codingbotlab@users.noreply.github.com>

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// File: src/main.cpp
+#include <iostream>
+#include "core/geometry/Geometry.h"
+
+int main() {
+    std::cout << "Hello, 3D Modeling Software!" << std::endl;
+
+    // Example usage of Geometry class
+    Geometry geometry;
+    geometry.draw();
+
+    return 0;
+}
